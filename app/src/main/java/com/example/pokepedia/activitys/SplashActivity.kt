dc1794e5428ac03442001947e53c3916ef9155ad
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-05-17 23:51:21 -0600

    Discard splash screen with style

--- app/src/main/java/com/example/pokepedia/activitys/SplashActivity.kt
+++ app/src/main/java/com/example/pokepedia/activitys/SplashActivity.kt
@@ -17,6 +17,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 5000)
+        }, 1000)
     }
 }
