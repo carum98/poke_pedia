ae4e6c0b589b263b1205c6eebe0048a924517c65
Irvin Matarrita <imbcr89@gmail.com>
2021-05-19 15:26:56 -0600

    Se agregan validaciones y en login

--- app/src/main/java/com/example/pokepedia/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/pokepedia/fragments/LoginFragment.kt
@@ -3,66 +3,124 @@ package com.example.pokepedia.fragments
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.pokepedia.R
 import com.example.pokepedia.databinding.FragmentLoginBinding
 import com.example.pokepedia.db.emtities.UserEntity
+import com.example.pokepedia.viewmodels.LoginFunViewModel
 import com.example.pokepedia.viewmodels.LoginViewModel
 import com.jakewharton.rxbinding4.view.focusChanges
 import com.jakewharton.rxbinding4.widget.checkedChanges
-import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxCompoundButton
+import com.jakewharton.rxbinding2.widget.RxRadioGroup
+import com.jakewharton.rxbinding2.widget.RxTextView
+import com.jakewharton.rxbinding4.widget.checked
+import io.reactivex.schedulers.Schedulers
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
-    private  val  viewModelLogin: LoginViewModel by viewModels()
+//    private  val  viewModelLogin: LoginViewModel by viewModels()
     private val disposable = CompositeDisposable()
+    lateinit var logicViewModel: LoginFunViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        logicViewModel= ViewModelProvider(this).get(LoginFunViewModel::class.java)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        disposable.add(
-            binding.name.focusChanges().subscribe{
-                hideKeyboard()
+        binding.btnLogin.isEnabled = false
 
-            }
+        disposable.add(
+            RxView.focusChanges(binding.name)
+                .subscribe{
+                    hideKeyboard()
+                }
         )
         disposable.add(
-            binding.gender.checkedChanges().subscribe{
-                hideKeyboard()
-                binding.name.clearFocus()
-            }
+            RxRadioGroup.checkedChanges(binding.gender)
+                .map { it}
+                .subscribe{
+                    hideKeyboard()
+                    binding.name.clearFocus()
+                    val gender = if(binding.gender.checkedRadioButtonId == R.id.male) "M" else "F"
+                    logicViewModel.inputs.gender.onNext(gender)
+                    logicViewModel.inputs.isChecked.onNext(binding.gender.checkedRadioButtonId != -1)
+                }
         )
-        binding.btnLogin.setOnClickListener {
+        disposable.add(
+            RxTextView.textChanges(binding.name)
+                .skipInitialValue()
+                .map { it.toString() }
+                .subscribe {
+                    logicViewModel.inputs.name.onNext(it)
+                }
+        )
+        disposable.add(
+            RxView.clicks(binding.btnLogin)
+                .subscribe{
+                    logicViewModel.inputs.saveClicked.onNext(Unit)
+                }
+        )
+
+        disposable.add(
+            logicViewModel.outputs.isButtonEnabled
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+           /*         if(binding.gender.checkedRadioButtonId<=0){//Grp is your radio group object
+                        binding.male.setError( "Selección requerida")
+                    }*/
+                    binding.btnLogin.isEnabled = it
+                }
+        )
+        disposable.add(
+            logicViewModel.outputs.nameError
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    binding.name.error = if (it) "Campo requerido" else null
+                }
+        )
+        disposable.add(
+            logicViewModel.outputs.insertNewUser
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    if(it){
+                        findNavController().popBackStack()
+                    }
+                }
+        )
+    /*    binding.btnLogin.setOnClickListener {
             val name = binding.name.text.toString()
-            val gender = if(binding.gender.checkedRadioButtonId == R.id.male) "M" else "F"
 
-            val user = UserEntity(name = name, gender = gender, isActive = true);
-            val userDB = viewModelLogin.findUser(user)
 
-            if (userDB != null) {
-                userDB.isActive = true
-                viewModelLogin.updateUser(userDB)
-            } else {
-                viewModelLogin.insertUser(user)
-            }
+            val user = UserEntity(name = name, gender = gender, isActive = true)
 
-            findNavController().popBackStack()
-        }
+
+
+        }*/
 
     }
     fun hideKeyboard() {
@@ -79,3 +137,5 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         _binding = null
     }
 }
+
+
---
+++ app/src/main/java/com/example/pokepedia/viewmodels/LoginFunViewModel.kt
@@ -0,0 +1,98 @@
+package com.example.pokepedia.viewmodels
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pokepedia.db.emtities.UserEntity
+import com.example.pokepedia.repositories.UserRepository
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+interface LoginFunViewModelInputs {
+    val name: Observer<String>
+    val gender: Observer<String>
+    val saveClicked: Observer<Unit>
+    val isChecked: Observer<Boolean>
+}
+
+interface LoginFunViewModelOutputs {
+    var isButtonEnabled: Observable<Boolean>
+    val nameError: Observable<Boolean>
+    val insertNewUser: Observable<Boolean>
+}
+
+interface LoginFunViewModelType {
+    val inputs: LoginFunViewModelInputs
+    val outputs: LoginFunViewModelOutputs
+}
+
+class LoginFunViewModel(application: Application) : AndroidViewModel(application),
+    LoginFunViewModelInputs,
+    LoginFunViewModelOutputs,
+    LoginFunViewModelType {
+  private var repository: UserRepository
+
+    // TYPE
+
+    override val inputs: LoginFunViewModelInputs = this
+    override val outputs: LoginFunViewModelOutputs = this
+
+    // INPUTS
+    override val name = BehaviorSubject.create<String>()
+    override val gender = BehaviorSubject.create<String>()
+    override val saveClicked = PublishSubject.create<Unit>()
+    override val isChecked = BehaviorSubject.create<Boolean>()
+
+
+    // OUTPUTS
+    override lateinit var isButtonEnabled: Observable<Boolean>
+    override val nameError: Observable<Boolean>
+    override val insertNewUser: Observable<Boolean>
+
+    init {
+        repository = UserRepository(application.applicationContext)
+        //val genero = if(binding.gender.checkedRadioButtonId == R.id.male) "M" else "F"
+
+        isButtonEnabled = Observable.combineLatest(name, isChecked, {n, g -> n.isNotEmpty() && g })
+
+        nameError = name.map { it.isEmpty() }
+
+        insertNewUser = saveClicked
+            .withLatestFrom(name, gender, { _, n, g: String -> UserEntity(n,g,true)})
+            .doOnNext {  acction(it) }
+            .map { true }
+
+
+
+    }
+    fun acction(user:UserEntity){
+        var userExitente= findUser(user)
+        if (userExitente != null) {
+            userExitente.isActive = true
+            updateUser(userExitente)
+        } else {
+            insertUser(user)
+        }
+    }
+  fun findUser(user: UserEntity) : UserEntity {
+      return repository.findUser(user);
+  }
+
+    fun insertUser(user: UserEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+                repository.insertUser(user)
+        }
+    }
+
+    fun updateUser(user: UserEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateUser(user)
+        }
+    }
+
+}
